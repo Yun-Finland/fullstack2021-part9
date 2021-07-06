5b963e2b282b5053926bc2ec14f4478e7c78c025
Yun Xiao <yun.xiao.finland@gmail.com>
2021-07-06 22:09:54 +0300

    ex9.19

--- ex9.8-9.13/patientor-backend/src/types.ts
+++ ex9.8-9.13/patientor-backend/src/types.ts
@@ -4,9 +4,44 @@ export interface DiagnoseEntry {
   latin?: string;
 } 
 
-export interface Entry {
-
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<string>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" =2,
+  "CriticalRisk"=3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck",
+  healthCheckRating: HealthCheckRating
+}
+
+export interface OccupationalHealthCareEntry extends BaseEntry{
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string,
+    endDate: string,
+  }
+}
+
+export interface HospitalEntry extends BaseEntry{
+  type: "Hospital",
+  discharge: {
+    date: string,
+    criteria:string,
+  }
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
 
 export enum Gender {
   Male = 'male',
